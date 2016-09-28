b71bf57aedbc6f4a805b5364d6d4a466a5bd89a6
rhiannonruth <rhiannonruth@hotmail.co.uk>
2016-09-28 16:37:04 +0100

    update tests attempting to follow betterspecs

--- spec/models/google_movies_scrape_spec.rb
+++ spec/models/google_movies_scrape_spec.rb
@@ -4,14 +4,14 @@ RSpec.describe GoogleMoviesScrape, type: :model do
 
   let!(:scraper){ GoogleMoviesScrape.new }
 
-  context "initialization" do
+  describe "#initialize" do
     it "creates a mechanize agent on init" do
       expect(scraper.agent).to_not be nil
     end
   end
 
-  context "scraping" do
-    it "returns an array" do
+  describe "#search_cinemas" do
+    it "returns hash" do
       response = scraper.search_cinemas("London")
       expect(response).to be_a Hash
     end
