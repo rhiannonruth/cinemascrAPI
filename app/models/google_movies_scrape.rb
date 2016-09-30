a53290b9d85abf9e1227f90ca721118dcc56c384
Paul <paulalexrees@gmail.com>
2016-09-30 19:39:55 +0100

    adds cinema object with name and array of movie titles

--- app/models/google_movies_scrape.rb
+++ app/models/google_movies_scrape.rb
@@ -8,14 +8,26 @@ class GoogleMoviesScrape
 
   def search_cinemas(location)
     page = agent.get("http://www.google.com/movies?near=#{location}")
-    { cinemas: get_cinema_names(page) }
+    { cinemas: get_cinema_info(page) }
   end
 
   private
 
-  def get_cinema_names(page)
-    page.search('.movie_results').search('.theater').search('.desc').search('.name').map do |name|
-      name.text
+  def get_cinema_info(page)
+    page.search('.movie_results').search('.theater').map do |cinema|
+      cinema_name = extract_name(cinema)
+      movies = extract_movies_list(cinema)
+      Cinema.new(name: cinema_name, movies:  movies)
+    end
+  end
+
+  def extract_name(cinema)
+    cinema.search('.desc').search('.name').text
+  end
+
+  def extract_movies_list(cinema)
+    cinema.search('.showtimes').search('.name').map do |movie|
+      movie.text
     end
   end
 
