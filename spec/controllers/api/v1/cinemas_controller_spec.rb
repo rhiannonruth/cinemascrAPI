6873b736c9131528033efedb852742defa5be49d
rhiannonruth <rhiannonruth@hotmail.co.uk>
2016-10-06 17:33:12 +0100

    improved current controller test'

--- spec/controllers/api/v1/cinemas_controller_spec.rb
+++ spec/controllers/api/v1/cinemas_controller_spec.rb
@@ -2,6 +2,8 @@ require 'rails_helper'
 
 RSpec.describe Api::V1::CinemasController, type: :controller do
 
+  let!(:cinema){ {"name"=>"ICA Cinema", "address"=>"The Mall, Institute of Contemporary Arts, London, United Kingdom", "telephone"=>"020 7930 3647", "movies"=>[{"title"=>"The Beatles: Eight Days a Week - The Touring Years", "length"=>"2hr 17min", "rating"=>"12A", "showtimes"=>["18:30"]}, {"title"=>"Little Men", "length"=>"1hr 25min", "rating"=>"PG", "showtimes"=>["14:00"]}, {"title"=>"Under the Shadow", "length"=>"1hr 24min", "rating"=>"15", "showtimes"=>["15:30", "20:55"]}, {"title"=>"De Palma", "length"=>"1hr 50min", "rating"=>"15", "showtimes"=>["16:00"]}]} }
+
   describe "GET #show" do
     it "returns http success" do
       get :show, id: "sw156sj"
@@ -10,11 +12,13 @@ RSpec.describe Api::V1::CinemasController, type: :controller do
   end
 
   describe "search" do
-    it "returns http success" do
-      get :search
-      json = JSON.parse(response.body)
-      expect(response).to have_http_status(:success)
-      expect(json.include?('cinemas')).to eq true
+    context 'search for London' do
+      it "success with expected results" do
+        get :search, location: 'London'
+        json = JSON.parse(response.body)
+        expect(response).to have_http_status(:success)
+        expect(json['cinemas'].include?(cinema)).to eq true
+      end
     end
   end
 
